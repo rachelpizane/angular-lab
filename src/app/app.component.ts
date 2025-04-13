5f2eee2ed88e975c1dbd3d0958c4ea9587cfd0a0
Rachel Pizane <rachel.pzm@gmail.com>
2025-04-13 11:30:06 -0300

    criado meu primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeiroComponenteComponent } from "./components/primeiro-componente/primeiro-componente.component";
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UpperCasePipe, PrimeiroComponenteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-lab';
+  title = 'Laboratório de Angular';
 }
---
+++ src/app/components/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primeiro-componente',
+  imports: [],
+  templateUrl: './primeiro-componente.component.html',
+  styleUrl: './primeiro-componente.component.css'
+})
+export class PrimeiroComponenteComponent {
+
+}
