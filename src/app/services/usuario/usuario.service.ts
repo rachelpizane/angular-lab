b0db06cc759308ed399d1e5dfbde9ea415ac9273
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-14 11:52:31 -0300

    feat: criado service para manipular API de usuarios

---
+++ src/app/services/usuario/usuario.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../../usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  private http: HttpClient = inject(HttpClient);
+  private apiUrl: string = 'https://jsonplaceholder.typicode.com/users';
+  constructor() { }
+
+  getAll() : Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.apiUrl);
+  }
+}
