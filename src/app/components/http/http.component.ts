cefd122213b43bc508b653dd9706fa439be6aaba
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-14 11:52:55 -0300

    feat: criado componente para exibir os usuarios

---
+++ src/app/components/http/http.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Usuario } from '../../usuario';
+import { UsuarioService } from '../../services/usuario/usuario.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-http',
+  imports: [CommonModule],
+  templateUrl: './http.component.html',
+  styleUrl: './http.component.css'
+})
+export class HttpComponent implements OnInit {
+  private usuarioService = inject(UsuarioService);
+  loading: boolean = false;
+  usuarios: Usuario[] = [];
+
+  ngOnInit(): void {
+    this.retornarTodosUsuarios()
+  }
+
+  retornarTodosUsuarios(): void {
+    this.loading = true;
+    this.usuarioService.getAll().subscribe((usuarios: Usuario[]) => {
+      this.usuarios = usuarios;
+      this.loading = false;
+    });
+  }
+}
