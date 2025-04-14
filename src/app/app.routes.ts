c8f4976c6e3856375e6d1dd142e361273fccc93d
Rachel Pizane <rachel.pzm@gmail.com>
2025-04-14 07:08:43 -0300

    refactor: Rota atualizada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { PaginaNaoEncontradaComponent } from './components/pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { PrimeiroComponenteComponent } from './components/primeiro-componente/primeiro-componente.component';
+import { ProdutoComponent } from './components/produto/produto.component';
 
 export const routes: Routes = [
   {path: '', component: PrimeiroComponenteComponent},
-  {path: 'usuarios', component: PaginaNaoEncontradaComponent},
+  {path: 'produtos', component: ProdutoComponent},
   {path: '*', component: PaginaNaoEncontradaComponent}
 ];
