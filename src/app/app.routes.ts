f8b6e90efc14852b4971bb50733ccccf60e720b1
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-14 11:51:53 -0300

    feat: inclusao da nova rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { PaginaNaoEncontradaComponent } from './components/pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { PrimeiroComponenteComponent } from './components/primeiro-componente/primeiro-componente.component';
 import { ProdutoComponent } from './components/produto/produto.component';
+import { HttpComponent } from './components/http/http.component';
 
 export const routes: Routes = [
   {path: '', component: PrimeiroComponenteComponent},
   {path: 'produtos', component: ProdutoComponent},
+  {path: 'usuarios', component: HttpComponent},
   {path: '*', component: PaginaNaoEncontradaComponent}
 ];
